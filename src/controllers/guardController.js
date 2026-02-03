2666bb9487203c8b1d5ed8dc1a62353ee0dfb433
Bhumika Saxena <bhumikasaxena4590@gmail.com>
2026-02-03 12:46:42 +0530

    Feat: Add profile_photo and documents to Add Guard response

--- src/controllers/guardController.js
+++ src/controllers/guardController.js
@@ -81,12 +81,14 @@ export const addGuard = async (req, res) => {
         }
 
         // Insert Documents
+        const uploadedDocuments = [];
         if (req.files && req.files["documents"]) {
             for (const file of req.files["documents"]) {
                 await client.query(
                     "INSERT INTO documents (guard_id, file_path, original_name) VALUES ($1, $2, $3)",
                     [guardId, file.filename, file.originalname]
                 );
+                uploadedDocuments.push({ filename: file.filename, original_name: file.originalname });
             }
         }
 
@@ -110,7 +112,9 @@ export const addGuard = async (req, res) => {
         return successResponse(res, "Guard added successfully", {
             guardId,
             supervisorId: req.user ? req.user.id : null,
-            supervisorName
+            supervisorName,
+            profile_photo: profile_photo,
+            documents: uploadedDocuments
         }, 201); // 201 Created
 
     } catch (error) {
